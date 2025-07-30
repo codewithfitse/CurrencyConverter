ef6bb791364417d3e652283461b906654eda287d
Fitsum Zerihun <fitsa0909@gmail.com>
2025-07-30 14:35:28 +0300

    HOMEPAGE

--- Frontend/src/page/Home.jsx
+++ Frontend/src/page/Home.jsx
@@ -6,17 +6,24 @@ export const Home = () => {
   const EURRATE = 0.0157;
   const CADRATE = 0.0969;
   const GBPRATE = 0.0177;
-  const ETHRATE = 134;
+  const ETHRATE = 0.0134;
 
+  const [searchInput1, setSearchInput1] = useState(null);
+  const [search1, setSearch1] = useState(null);
   const [searchInput, setSearchInput] = useState(null);
   const [search, setSearch] = useState(null);
+
+  const handleClick1 = (e) => {
+    e.preventDefault();
+    const calc = Number((searchInput * 9) / 5 + 32);
+    setSearch1(calc);
+  };
 
   const handleClick = (e) => {
     e.preventDefault();
     const calc = Number((searchInput * (ETHRATE * 1000)) / 1000);
     setSearch(calc);
   };
-
   return (
     <>
       <div
@@ -38,6 +45,31 @@ export const Home = () => {
               className="w-full p-3 rounded-2xl bg-accent text-black"
               placeholder="Enter Currency"
               onChange={(e) => setSearchInput(e.target.value)}
+            />
+            <button
+              type="submit"
+              className="p-3 rounded-[10px] bg-accent text-black"
+            >
+              Submit
+            </button>
+          </form>
+        </div>
+        <div className="mt-10 w-[90%] h-[90%] p-5 flex flex-col items-center space-y-3 bg-white/5 backdrop-blur-2xl border-2 border-accent">
+          <h1 className="">Temperature Converter</h1>
+          <p className="">1°C = 33.8°F</p>
+          <h2 className="">
+            C - {searchInput1} To F - {search1}
+          </h2>
+          <form
+            action=""
+            className="w-full flex justify-between items-center space-x-2"
+            onSubmit={handleClick1}
+          >
+            <input
+              type="text"
+              className="w-full p-3 rounded-2xl bg-accent text-black"
+              placeholder="Enter Currency"
+              onChange={(e) => setSearchInput1(e.target.value)}
             />
             <button
               type="submit"
