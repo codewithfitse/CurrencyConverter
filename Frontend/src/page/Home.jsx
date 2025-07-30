5dea85c575688c1e92c81c635649d795db95dab6
Fitsum Zerihun <fitsa0909@gmail.com>
2025-07-30 14:13:11 +0300

    HOMEPAGE

--- Frontend/src/page/Home.jsx
+++ Frontend/src/page/Home.jsx
@@ -1,7 +1,18 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
+//import axios from "axios";
 
 export const Home = () => {
+  const USDRATE = 0.12;
+
+  const [searchInput, setSearchInput] = useState(null);
+  const [search, setSearch] = useState(null);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    const calc = Number((searchInput * (USDRATE * 100)) / 100);
+    setSearch(calc);
+  };
+
   return (
     <>
       <div
@@ -9,17 +20,26 @@ export const Home = () => {
       >
         <div className="mt-10 w-[90%] h-[90%] p-5 flex flex-col items-center space-y-3 bg-white/5 backdrop-blur-2xl border-2 border-accent">
           <h1 className="">Currency Converter</h1>
-          <h2 className="">To USD- </h2>
-          <div className="w-full flex justify-between items-center space-x-2">
+          <p className="">1 USD = 120ET</p>
+          <h2 className="">To USD- {search}</h2>
+          <form
+            action=""
+            className="w-full flex justify-between items-center space-x-2"
+            onSubmit={handleClick}
+          >
             <input
               type="text"
               className="w-full p-3 rounded-2xl bg-accent text-black"
               placeholder="Enter Currency"
+              onChange={(e) => setSearchInput(e.target.value)}
             />
-            <button className="p-3 rounded-[10px] bg-accent text-black">
+            <button
+              type="submit"
+              className="p-3 rounded-[10px] bg-accent text-black"
+            >
               Submit
             </button>
-          </div>
+          </form>
         </div>
       </div>
     </>
