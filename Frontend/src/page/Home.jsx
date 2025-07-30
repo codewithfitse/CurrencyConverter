a2987635657bc8777568b6c44e726aa2ec85d2a9
Fitsum Zerihun <fitsa0909@gmail.com>
2025-07-30 20:34:27 +0300

    HOMEPAGE

--- Frontend/src/page/Home.jsx
+++ Frontend/src/page/Home.jsx
@@ -151,7 +151,7 @@ export const Home = () => {
 
               <div className="flex justify-between mt-6">
                 <button
-                  className="px-4 py-2 rounded-lg bg-gray-700 hover:bg-gray-600 disabled:opacity-50 "
+                  className="px-4 py-2 rounded-lg bg-gray-700 hover:bg-gray-600 disabled:opacity-50"
                   disabled={!isCompleted}
                   onClick={() => alert("Back clicked")}
                 >
