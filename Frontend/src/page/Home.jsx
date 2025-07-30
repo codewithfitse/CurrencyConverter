a60026a42bd82633c91105c48c7757aff3f86299
Fitsum Zerihun <fitsa0909@gmail.com>
2025-07-30 13:58:56 +0300

    HOMEPAGE

--- Frontend/src/page/Home.jsx
+++ Frontend/src/page/Home.jsx
@@ -1,12 +1,5 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import WindyIcon, {
-  HumidIcon,
-  SnowIcon,
-  SunnyCloudyIcon,
-  SunnyIcon,
-  SunnyRainyIcon,
-} from "../componet/Windy";
 
 export const Home = () => {
   return (
@@ -14,8 +7,16 @@ export const Home = () => {
       <div
         className={`w-full min-h-screen bg-gray-900 text-white flex flex-col items-center overflow-x-hidden`}
       >
-        <div className="mt-10 w-[90%] h-[90%] p-5 bg-white/5 backdrop-blur-2xl border-2 border-accent">
-          <h1 className="">Curency converter</h1>
+        <div className="mt-10 w-[90%] h-[90%] p-5 flex flex-col items-center space-y-3 bg-white/5 backdrop-blur-2xl border-2 border-accent">
+          <h1 className="">Currency Converter</h1>
+          <h2 className="">To USD- </h2>
+          <div className="w-full flex justify-between items-center space-x-2">
+            <input
+              type="text"
+              className="w-full p-3 rounded-2xl bg-accent text-black"
+            />
+            <button className="p-3 rounded-[10px] bg-accent">Submit</button>
+          </div>
         </div>
       </div>
     </>
