91b0329f9f2b9f0969b26e06884a33acdf8fdff0
Fitsum Zerihun <fitsa0909@gmail.com>
2025-07-30 14:20:31 +0300

    HOMEPAGE

--- Frontend/src/page/Home.jsx
+++ Frontend/src/page/Home.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 //import axios from "axios";
 
 export const Home = () => {
-  const USDRATE = 1.2;
+  const USDRATE = 0.012;
 
   const [searchInput, setSearchInput] = useState(null);
   const [search, setSearch] = useState(null);
@@ -21,7 +21,9 @@ export const Home = () => {
         <div className="mt-10 w-[90%] h-[90%] p-5 flex flex-col items-center space-y-3 bg-white/5 backdrop-blur-2xl border-2 border-accent">
           <h1 className="">Currency Converter</h1>
           <p className="">1 USD = 120ET</p>
-          <h2 className="">To USD- {search}</h2>
+          <h2 className="">
+            Eth - {searchInput} To USD - {search}
+          </h2>
           <form
             action=""
             className="w-full flex justify-between items-center space-x-2"
